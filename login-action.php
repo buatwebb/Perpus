cda9a9d0f26a75f6e1454e1e12e0b7f667089b31
Fikri Ramdani <fikriramdani977@gmail.com>
2025-02-07 20:53:53 +0700

    Action Login

--- login-action.php
+++ login-action.php
@@ -7,8 +7,8 @@ session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$query = "SELECT * FROM tb_user WHERE username = '$username' AND password = '$password";
-$login  = mysqli_query($connection, $query);
+$query = "SELECT * FROM tpengguna WHERE username = '$username' AND password = '$password'";
+$login = mysqli_query($conn, $query);
 
 $cek = mysqli_num_rows($login);
 
@@ -16,10 +16,26 @@ if ($cek > 0) {
     $data = mysqli_fetch_assoc($login);
     if ($data['password'] == $password) {
         $_SESSION['username'] = $data['username'];
-        $_SESSION['password'] = $data['password'];
+        $_SESSION['id_pengguna'] = $data['id_pengguna'];
+        $_SESSION['email'] = $data['email'];
+        $_SESSION['telp'] = $data['telp'];
+        $_SESSION['alamat'] = $data['alamat'];
         
-        
+        if ($username === 'admin' && $password === 'admin') {
+            header("Location: Admin/index.php");
+        } else {
+            header("Location: User/index.php");
+        }
+        exit();
+    } else {
+        $message = "Username atau Password salah!";
+        echo "<script>alert('$message');</script>";
+        echo "<script>window.location.href='login.php';</script>";
     }
+} else {
+    $message = "Username atau Password salah!";
+    echo "<script>alert('$message');</script>";
+    echo "<script>window.location.href='login.php';</script>";
+    exit();
 }
-
 ?>
